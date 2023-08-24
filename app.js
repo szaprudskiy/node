f9687aaa667daaadb0334c8ecf1f543816d24f7a
szaprudskiy <sergei.zaprudskiy@gmail.com>
2023-08-24 12:55:12 +0300

    443

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const router = require('./routes/index.js')
 require('dotenv').config()
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 443
 const app = express()
 
 app.use(express.json())
